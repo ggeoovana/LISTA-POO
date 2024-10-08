c095f3628c84fe6a175f1a12bf0a783434dc9d7e
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-10-08 16:03:14 -0300

    Add files via upload

---
+++ Lista1Ex10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Terreno t;
+            t = new Terreno();
+
+            Console.WriteLine(" Digite o valor da base");
+            t.setBase(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine(" Digite o valor da altura");
+            t.setAltura(double.Parse(Console.ReadLine()));
+
+            t.calcularArea();
+            Console.WriteLine("Area é: {0} - {1}", t.getArea(), t.getMaiorExtenso());
+
+
+
+
+        }
+    }
+}
